b0f1d8734be5ccb0bd7b6419db703d1e7b8c63e9
Zack Adlington <zadlington@gmail.com>
2020-07-06 15:26:15 +0100

    merged accordion.css to main.css

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
           $("#group-msg").html(`
             <p>Great news! A local Liveable Neighbourhoods group is active in ${ward}:</p>
             <div id='facebook-link'>
-              <img id="fb-logo" src="/assets/fb_logo.png" alt="Facebook logo" />
+              <img id="fb-logo" src="/assets/fb-logo.png" alt="Facebook logo" />
               <a href="${ln_groups[ward].url}">${ln_groups[ward].name}</a>
             </div>
           `);
@@ -49,6 +49,12 @@ $(document).ready(function () {
       ).html(`<p>We're having difficulty finding your postcode.  Is it definitely correct?</p>
           <p>You can get in touch directly with <a href="mailto:info@bristolcycling.org.uk">info@bristolcycling.org.uk</a> and we'll be able to help you find your local group.`);
     });
+  });
+  $("#postcode-input").keypress(function (e) {
+    if (e.which == 13) {
+      // Enter key pressed
+      $("#postcode-search-btn").click();
+    }
   });
 });
 
