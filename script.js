d27166fd2ae5a4c0da0ed76984c87250e578eb00
Zack Adlington <zadlington@gmail.com>
2020-07-06 11:09:16 +0100

    Added submit btn to group lookup

--- script.js
+++ script.js
@@ -4,69 +4,55 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
 
 // # Postcode -> ward lookup
-let input = document.getElementById("postcode-input");
-let output = document.getElementById("group-msg");
+$(document).ready(function () {
+  $("#postcode-search-btn").click(function (event) {
+    let postcode = $("#postcode-input").val();
 
-// Only query and display FB group after user finishes typing
-let input_timeout = null;
-
-input.addEventListener("keyup", function (e) {
-  clearTimeout(input_timeout);
-
-  input_timeout = setTimeout(function () {
-    console.log("Input Value:", input.value);
-
-    const postcode = input.value;
-    const request = new XMLHttpRequest();
-
-    request.open("GET", `https://api.postcodes.io/postcodes/${postcode}`, true);
-
-    request.onload = function () {
-      const postcode_data = JSON.parse(this.response);
-
-      if (request.status >= 200 && request.status < 400) {
-        const ward = postcode_data.result.admin_ward;
-        console.log(`ward = ${ward}, url = ${groups[ward]}`);
-        output.innerHTML = `<p>Great news! A local Liveable Neighbourhoods group is active in ${ward}:</p>
-          <div>
-            <img id="fb-logo" src="/assets/fb_logo.png" alt="Facebook logo" />
-            <a href="${groups[ward].url}">${groups[ward].name}</a>
-          </div>`;
-      } else {
-        output.innerHTML = `<p>No group currently exists for your area. Why don't you set one up?</p>
-        <p>If you need support, speak to <a href="mailto:info@bristolcycling.org.uk">info@bristolcycling.org.uk</a>`;
-      }
-    };
-    request.send();
-  }, 1000);
+    // Get ward from postcode
+    $.getJSON(`https://api.postcodes.io/postcodes/${postcode}`, function (
+      postcode_details
+    ) {
+      let ward = postcode_details.result.admin_ward;
+      // Get Facebook group name and link from ward
+      $.getJSON("ln_groups.json", function (ln_groups) {
+        if (ln_groups[ward]) {
+          $("#group-msg").html(`
+            <p>Great news! A local Liveable Neighbourhoods group is active in ${ward}:</p>
+            <div id='facebook-link'>
+              <img id="fb-logo" src="/assets/fb_logo.png" alt="Facebook logo" />
+              <a href="${ln_groups[ward].url}">${ln_groups[ward].name}</a>
+            </div>
+          `);
+        } else {
+          $("#group-msg")
+            .html(`<p>No group currently exists for your area. Why don't you set one up?</p>
+          <p>If you need support, speak to <a href="mailto:info@bristolcycling.org.uk">info@bristolcycling.org.uk</a>`);
+        }
+      });
+    }).fail(function (d, textStatus, error) {
+      console.error(
+        "getJSON failed, status: " + textStatus + ", error: " + error
+      );
+      $(
+        "#group-msg"
+      ).html(`<p>We're having difficulty finding your postcode.  Is it definitely correct?</p>
+          <p>You can get in touch directly with <a href="mailto:info@bristolcycling.org.uk">info@bristolcycling.org.uk</a> and we'll be able to help you find your local group.`);
+    });
+  });
 });
 
-const groups = {
-  Cotham: {
-    name: "Cotham Liveable Streets",
-    url: "https://en-gb.facebook.com/cothams/",
-  },
-  "Lawrence Hill": {
-    name: "Liveable Lawrence Hill",
-    url: "https://www.facebook.com/pages/Lawrence-Hill-Bristol/242077265821144",
-  },
-  "Windmill Hill": {
-    name: "Walkable Windmill Hill",
-    url: "https://en-gb.facebook.com/groups/WMH.VP/",
-  },
-};
+// Prevent 'enter' keypress from submitting form
+$("#postcode-lookup").submit(function () {
+  return false;
+});
