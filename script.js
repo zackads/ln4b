c3ca1eade5add2ac48d63ca2c62e61d6a1bbac64
Zack Adlington <zadlington@gmail.com>
2020-07-14 19:44:58 +0100

    Completed groupfinder block component

--- script.js
+++ script.js
@@ -32,10 +32,12 @@ $(document).ready(function () {
             <img class="groupfinder__social-logo" id="fb-logo" src="/assets/images/fb-logo.png" alt="Facebook logo" />
             <a href="${ln_groups[ward].url}">${ln_groups[ward].name}</a>
           `);
+          $("#group-msg").css("display", "inline-flex");
         } else {
           $("#group-msg")
             .html(`<p>No group currently exists for your area. Why don't you set one up?</p>
           <p>If you need support, speak to <a href="mailto:info@bristolcycling.org.uk">info@bristolcycling.org.uk</a>`);
+          $("#group-msg").css("display", "inline-flex");
         }
       });
     }).fail(function (d, textStatus, error) {
